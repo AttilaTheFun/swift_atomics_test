d9a258addc654c7803d60da4a21496304f592b9b
Logan Shire <logan.shire@gmail.com>
2023-06-04 16:26:34 -0400

    Update

--- Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
+++ Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
@@ -46,15 +46,15 @@ where Value.AtomicRepresentation.Value == Value {
     _storage = _Storage(value)
   }
 
-  deinit {
-    _ = _ptr.pointee.dispose()
-  }
+  // deinit {
+  //   _ = _ptr.pointee.dispose()
+  // }
 
-  @_alwaysEmitIntoClient @inline(__always)
-  internal var _ptr: UnsafeMutablePointer<_Storage> {
-    _getUnsafePointerToStoredProperties(self)
-      .assumingMemoryBound(to: _Storage.self)
-  }
+  // @_alwaysEmitIntoClient @inline(__always)
+  // internal var _ptr: UnsafeMutablePointer<_Storage> {
+  //   _getUnsafePointerToStoredProperties(self)
+  //     .assumingMemoryBound(to: _Storage.self)
+  // }
 }
 
 @main

cf4440e310109b18cc4251d8d4fc1808c21d4fb1
Logan Shire <logan.shire@gmail.com>
2023-06-04 16:16:51 -0400

    Update flags

--- Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
+++ Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
@@ -1,12 +1,13 @@
-import Atomics
+// import Atomics
+import _AtomicsShims
 
 @main
 struct SwiftAtomicsTestMain {
   static func main() throws {
     print("before atomic")
 
-    let isOpen = ManagedAtomic(false)
-    print(isOpen.load(ordering: .sequentiallyConsistent))
+    let atomicValue: Int8 = 1
+    let storage = _sa_prepare_Int8(atomicValue)
 
     print("after atomic")
   }

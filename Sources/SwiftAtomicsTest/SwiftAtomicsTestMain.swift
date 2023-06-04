3b2af87de53ffc69bceb65944f86dbb130bcae96
Logan Shire <logan.shire@gmail.com>
2023-06-04 16:23:56 -0400

    Update

--- Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
+++ Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
@@ -1,25 +1,25 @@
-// import Atomics
+import Atomics
 import _AtomicsShims
 
-extension Bool {
-  struct TestAtomicRepresentation {
-    @usableFromInline
-    internal typealias _Storage = _AtomicInt8Storage
+// extension Bool {
+//   struct TestAtomicRepresentation {
+//     @usableFromInline
+//     internal typealias _Storage = _AtomicInt8Storage
 
-    @usableFromInline
-    internal var _storage: _Storage
+//     @usableFromInline
+//     internal var _storage: _Storage
 
-    @inline(__always) @_alwaysEmitIntoClient
-    public init(_ value: Bool) {
-      _storage = _sa_prepare_Int8(value._atomicValue)
-    }
-  }
+//     @inline(__always) @_alwaysEmitIntoClient
+//     public init(_ value: Bool) {
+//       _storage = _sa_prepare_Int8(value._atomicValue)
+//     }
+//   }
 
-  @_alwaysEmitIntoClient @inline(__always)
-  internal var _atomicValue: Int8 {
-    self ? 1 : 0
-  }
-}
+//   @_alwaysEmitIntoClient @inline(__always)
+//   internal var _atomicValue: Int8 {
+//     self ? 1 : 0
+//   }
+// }
 
 @main
 struct SwiftAtomicsTestMain {
@@ -27,7 +27,7 @@ struct SwiftAtomicsTestMain {
     print("before atomic")
 
     let bool = true
-    let storage = Bool.TestAtomicRepresentation(bool)
+    let storage = Bool.AtomicRepresentation(bool)
 
     print("after atomic")
   }

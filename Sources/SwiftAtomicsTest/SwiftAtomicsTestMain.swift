9a7be8a1f974088a6bcb47ee7339ef087d59fcbb
Logan Shire <logan.shire@gmail.com>
2023-06-04 16:21:33 -0400

    Update

--- Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
+++ Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
@@ -1,13 +1,33 @@
 // import Atomics
 import _AtomicsShims
 
+extension Bool {
+  struct TestAtomicRepresentation {
+    @usableFromInline
+    internal typealias _Storage = _AtomicInt8Storage
+
+    @usableFromInline
+    internal var _storage: _Storage
+
+    @inline(__always) @_alwaysEmitIntoClient
+    public init(_ value: Bool) {
+      _storage = _sa_prepare_Int8(value._atomicValue)
+    }
+  }
+
+  @_alwaysEmitIntoClient @inline(__always)
+  internal var _atomicValue: Int8 {
+    self ? 1 : 0
+  }
+}
+
 @main
 struct SwiftAtomicsTestMain {
   static func main() throws {
     print("before atomic")
 
-    let atomicValue: Int8 = 1
-    let storage = _sa_prepare_Int8(atomicValue)
+    let bool = true
+    let storage = Bool.TestAtomicRepresentation(bool)
 
     print("after atomic")
   }

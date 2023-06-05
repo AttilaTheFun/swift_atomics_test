cac072031f8b2b364341a6e309ef592c9dd84b86
Logan Shire <logan.shire@gmail.com>
2023-06-04 22:14:22 -0400

    Update

--- Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
+++ Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
@@ -62,14 +62,14 @@ extension Bool: AtomicValue {
     public typealias Value = Bool
 
     @usableFromInline
-    internal typealias _Storage = Int8 // _AtomicInt8Storage
+    internal typealias _Storage = _AtomicInt8Storage
 
     @usableFromInline
     internal var _storage: _Storage
 
     @inline(__always) @_alwaysEmitIntoClient
     public init(_ value: Bool) {
-      _storage = value._atomicValue
+      _storage = _sa_prepare_Int8(value._atomicValue)
     }
   }
 

754a9a645226db2d39bd771086981078d97ba700
Logan Shire <logan.shire@gmail.com>
2023-06-04 22:09:00 -0400

    Update

--- Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
+++ Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
@@ -1,50 +1,106 @@
-import Atomics
-import _AtomicsShims
+// import Atomics
+// import _AtomicsShims
 
 // extension Bool {
-//   struct TestAtomicRepresentation {
-//     @usableFromInline
-//     internal typealias _Storage = _AtomicInt8Storage
+  // struct TestAtomicRepresentation {
+  //   @usableFromInline
+  //   internal typealias _Storage = _AtomicInt8Storage
 
-//     @usableFromInline
-//     internal var _storage: _Storage
+  //   @usableFromInline
+  //   internal var _storage: _Storage
 
-//     @inline(__always) @_alwaysEmitIntoClient
-//     public init(_ value: Bool) {
-//       _storage = _sa_prepare_Int8(value._atomicValue)
-//     }
-//   }
+  //   @inline(__always) @_alwaysEmitIntoClient
+  //   public init(_ value: Bool) {
+  //     _storage = _sa_prepare_Int8(value._atomicValue)
+  //   }
+  // }
 
-//   @_alwaysEmitIntoClient @inline(__always)
-//   internal var _atomicValue: Int8 {
-//     self ? 1 : 0
+  // @_alwaysEmitIntoClient @inline(__always)
+  // internal var _atomicValue: Int8 {
+  //   self ? 1 : 0
+  // }
+// }
+
+// @_fixed_layout
+// public class TestManagedAtomic<Value: AtomicValue>
+// where Value.AtomicRepresentation.Value == Value {
+//   // Note: the Value.AtomicRepresentation.Value == Value requirement could be relaxed,
+//   // at the cost of adding a bunch of potentially ambiguous overloads.
+//   // (We'd need one set of implementations for the type equality condition,
+//   // and another for `Value: AtomicReference`.)
+
+//   @usableFromInline
+//   internal typealias _Storage = Value.AtomicRepresentation
+
+//   /// The atomic representation of the value stored inside.
+//   ///
+//   /// Warning: This ivar must only ever be accessed via `_ptr` after
+//   /// its initialization.
+//   @usableFromInline
+//   internal var _storage: _Storage
+
+//   /// Initialize a new managed atomic instance holding the specified initial
+//   /// value.
+//   @inline(__always) @_alwaysEmitIntoClient
+//   public init(_ value: Value) {
+//     _storage = _Storage(value)
 //   }
 // }
 
+public protocol AtomicValue {
+  associatedtype AtomicRepresentation: AtomicStorage
+}
+
+public protocol AtomicStorage {
+  associatedtype Value
+
+  init(_ value: __owned Value)
+}
+
+extension Bool: AtomicValue {
+  public struct AtomicRepresentation: AtomicStorage {
+    @usableFromInline
+    internal typealias _Storage = Int8 // _AtomicInt8Storage
+
+    @usableFromInline
+    internal var _storage: _Storage
+
+    @inline(__always) @_alwaysEmitIntoClient
+    public init(_ value: Bool) {
+      _storage = value._atomicValue
+    }
+  }
+
+  @_alwaysEmitIntoClient @inline(__always)
+  internal var _atomicValue: Int8 {
+    self ? 1 : 0
+  }
+}
+
 @_fixed_layout
-public class TestManagedAtomic<Value: AtomicValue>
+public class ManagedAtomic<Value: AtomicValue>
 where Value.AtomicRepresentation.Value == Value {
-  // Note: the Value.AtomicRepresentation.Value == Value requirement could be relaxed,
-  // at the cost of adding a bunch of potentially ambiguous overloads.
-  // (We'd need one set of implementations for the type equality condition,
-  // and another for `Value: AtomicReference`.)
 
   @usableFromInline
   internal typealias _Storage = Value.AtomicRepresentation
 
-  /// The atomic representation of the value stored inside.
-  ///
-  /// Warning: This ivar must only ever be accessed via `_ptr` after
-  /// its initialization.
   @usableFromInline
   internal var _storage: _Storage
 
-  /// Initialize a new managed atomic instance holding the specified initial
-  /// value.
   @inline(__always) @_alwaysEmitIntoClient
   public init(_ value: Value) {
     _storage = _Storage(value)
   }
+
+  // deinit {
+  //   _ = _ptr.pointee.dispose()
+  // }
+
+  // @_alwaysEmitIntoClient @inline(__always)
+  // internal var _ptr: UnsafeMutablePointer<_Storage> {
+  //   _getUnsafePointerToStoredProperties(self)
+  //     .assumingMemoryBound(to: _Storage.self)
+  // }
 }
 
 @main
@@ -54,7 +110,7 @@ struct SwiftAtomicsTestMain {
 
     // let bool = true
     // let storage = Bool.AtomicRepresentation(bool)
-    let managedAtomic = TestManagedAtomic(false)
+    let managedAtomic = ManagedAtomic(false)
 
     print("after atomic")
   }

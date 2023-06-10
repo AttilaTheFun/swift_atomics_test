6f279535c75dffe8e21cf4856cc8ac40782212cf
Logan Shire <logan.shire@gmail.com>
2023-06-10 19:39:12 -0400

    Commented out more code

--- Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
+++ Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift
@@ -1,12 +1,10 @@
-// import Atomics
+import Atomics
 
 @main
 struct SwiftAtomicsTestMain {
   static func main() throws {
     print("before atomic")
 
-    // let bool = true
-    // let storage = Bool.AtomicRepresentation(bool)
     let managedAtomic = ManagedAtomic(false)
 
     print("after atomic")

30bae39543721fd8e9f66cd377963e3d1ce3e0ed
Logan Shire <logan.shire@gmail.com>
2023-06-17 14:30:59 -0400

    Trying to repro swift build

--- shell-bazel/FixBazelSegfaultOpt.sh
+++ shell-bazel/FixBazelSegfaultOpt.sh
@@ -159,7 +159,7 @@ bazel-out-test/aarch64-opt/bin/Sources/Atomics/Atomics_objs/ManagedAtomic.swift.
 -lm \
 # -lstdc++ \
 # -lrt \
--ldl \
+# -ldl \
 /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/lib/swift/linux/aarch64/swiftrt.o \
 -static-libgcc \
 # -lstdc++ \

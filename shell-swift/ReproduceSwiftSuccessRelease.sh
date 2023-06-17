7349812a937f179ed3b57ebfcee2e9291c19febf
Logan Shire <logan.shire@gmail.com>
2023-06-17 14:14:10 -0400

    Trying to repro swift build

--- shell-swift/ReproduceSwiftSuccessRelease.sh
+++ shell-swift/ReproduceSwiftSuccessRelease.sh
@@ -21,10 +21,11 @@ mkdir -p .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build
 # touch .build/aarch64-unknown-linux-gnu/release/Atomics.build/Atomics_objs/AtomicValue.swift.o
 # touch .build/aarch64-unknown-linux-gnu/release/Atomics.build/Atomics_objs/ManagedAtomic.swift.ast
 # touch .build/aarch64-unknown-linux-gnu/release/Atomics.build/Atomics_objs/ManagedAtomic.swift.o
+touch .build/aarch64-unknown-linux-gnu/release/Atomics.build/Atomics.autolink
 # touch .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTest.output_file_map.json
 # touch .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTest_objs/SwiftAtomicsTestMain.swift.ast
 # touch .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTest_objs/SwiftAtomicsTestMain.swift.o
-# touch .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTest.autolink
+touch .build/aarch64-unknown-linux-gnu/release/Atomics.build/SwiftAtomicsTest.autolink
 # touch .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTest.swiftmodule
 
 # Copying Output File Maps
@@ -46,19 +47,19 @@ Atomics \
 -emit-dependencies \
 -emit-module \
 -emit-module-path \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.swiftmodule \
+.build/aarch64-unknown-linux-gnu/release/Atomics.swiftmodule \
 -output-file-map \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/output-file-map.json \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/output-file-map.json \
 -parse-as-library \
 -whole-module-optimization \
 -num-threads \
 2 \
 -c \
-/home/ubuntu/swift_atomics_test/Sources/Atomics/AtomicBool.swift \
-/home/ubuntu/swift_atomics_test/Sources/Atomics/AtomicValue.swift \
-/home/ubuntu/swift_atomics_test/Sources/Atomics/ManagedAtomic.swift \
+Sources/Atomics/AtomicBool.swift \
+Sources/Atomics/AtomicValue.swift \
+Sources/Atomics/ManagedAtomic.swift \
 -I \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release \
+.build/aarch64-unknown-linux-gnu/release \
 -target \
 aarch64-unknown-linux-gnu \
 -swift-version \
@@ -68,7 +69,7 @@ aarch64-unknown-linux-gnu \
 -j2 \
 -DSWIFT_PACKAGE \
 -module-cache-path \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/ModuleCache \
+.build/aarch64-unknown-linux-gnu/release/ModuleCache \
 -parseable-output \
 -parse-as-library \
 -v
@@ -78,9 +79,9 @@ aarch64-unknown-linux-gnu \
 # /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/bin/swift-frontend \
 # -frontend \
 # -c \
-# /home/ubuntu/swift_atomics_test/Sources/Atomics/AtomicBool.swift \
-# /home/ubuntu/swift_atomics_test/Sources/Atomics/AtomicValue.swift \
-# /home/ubuntu/swift_atomics_test/Sources/Atomics/ManagedAtomic.swift \
+# Sources/Atomics/AtomicBool.swift \
+# Sources/Atomics/AtomicValue.swift \
+# Sources/Atomics/ManagedAtomic.swift \
 # -supplementary-output-file-map \
 # /tmp/TemporaryDirectory.NXPY08/supplementaryOutputs-1 \
 # -target \
@@ -89,10 +90,10 @@ aarch64-unknown-linux-gnu \
 # -aarch64-use-tbi \
 # -disable-objc-interop \
 # -I \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release \
+# .build/aarch64-unknown-linux-gnu/release \
 # -g \
 # -module-cache-path \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/ModuleCache \
+# .build/aarch64-unknown-linux-gnu/release/ModuleCache \
 # -swift-version \
 # 5 \
 # -O \
@@ -110,22 +111,22 @@ aarch64-unknown-linux-gnu \
 # -num-threads \
 # 2 \
 # -o \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicBool.swift.o \
+# .build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicBool.swift.o \
 # -o \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicValue.swift.o \
+# .build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicValue.swift.o \
 # -o \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/ManagedAtomic.swift.o
+# .build/aarch64-unknown-linux-gnu/release/Atomics.build/ManagedAtomic.swift.o
 
 # Extracting autolink data for Swift module Atomics
 
 echo "Extracting autolink data for Swift module Atomics"
 
 /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/bin/swift-autolink-extract \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicBool.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicValue.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/ManagedAtomic.swift.o \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicBool.swift.o \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicValue.swift.o \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/ManagedAtomic.swift.o \
 -o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/Atomics.autolink
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/Atomics.autolink
 
 # Compiling Swift module SwiftAtomicsTest
 
@@ -138,16 +139,16 @@ SwiftAtomicsTest \
 -emit-dependencies \
 -emit-module \
 -emit-module-path \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.swiftmodule \
+.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.swiftmodule \
 -output-file-map \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/output-file-map.json \
+.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/output-file-map.json \
 -whole-module-optimization \
 -num-threads \
 2 \
 -c \
-/home/ubuntu/swift_atomics_test/Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift \
+Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift \
 -I \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release \
+.build/aarch64-unknown-linux-gnu/release \
 -target \
 aarch64-unknown-linux-gnu \
 -swift-version \
@@ -157,7 +158,7 @@ aarch64-unknown-linux-gnu \
 -j2 \
 -DSWIFT_PACKAGE \
 -module-cache-path \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/ModuleCache \
+.build/aarch64-unknown-linux-gnu/release/ModuleCache \
 -parseable-output \
 -Xfrontend \
 -entry-point-function-name \
@@ -171,25 +172,25 @@ SwiftAtomicsTest_main \
 # /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/bin/swift-frontend \
 # -frontend \
 # -c \
-# /home/ubuntu/swift_atomics_test/Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift \
+# Sources/SwiftAtomicsTest/SwiftAtomicsTestMain.swift \
 # -emit-module-path \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.swiftmodule \
+# .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.swiftmodule \
 # -emit-module-doc-path \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.swiftdoc \
+# .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.swiftdoc \
 # -emit-module-source-info-path \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.swiftsourceinfo \
+# .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.swiftsourceinfo \
 # -emit-dependencies-path \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTest.d \
+# .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTest.d \
 # -target \
 # aarch64-unknown-linux-gnu \
 # -Xllvm \
 # -aarch64-use-tbi \
 # -disable-objc-interop \
 # -I \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release \
+# .build/aarch64-unknown-linux-gnu/release \
 # -g \
 # -module-cache-path \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/ModuleCache \
+# .build/aarch64-unknown-linux-gnu/release/ModuleCache \
 # -swift-version \
 # 5 \
 # -O \
@@ -209,23 +210,23 @@ SwiftAtomicsTest_main \
 # -num-threads \
 # 2 \
 # -o \
-# /home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTestMain.swift.o
+# .build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTestMain.swift.o
 
 # Extracting autolink data for Swift module SwiftAtomicsTest
 
 echo "Extracting autolink data for Swift module SwiftAtomicsTest"
 
 /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/bin/swift-autolink-extract \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTestMain.swift.o \
+.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTestMain.swift.o \
 -o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTest.autolink
+.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTest.autolink
 
 /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/bin/swiftc \
 -g \
 -L \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release \
+.build/aarch64-unknown-linux-gnu/release \
 -o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest \
+.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest \
 -module-name \
 SwiftAtomicsTest \
 -emit-executable \
@@ -237,11 +238,11 @@ SwiftAtomicsTest \
 main=SwiftAtomicsTest_main \
 -Xlinker \
 '-rpath=$ORIGIN' \
-# @/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.product/Objects.LinkFileList \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicBool.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicValue.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/ManagedAtomic.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTestMain.swift.o \
+# @.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.product/Objects.LinkFileList \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicBool.swift.o \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicValue.swift.o \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/ManagedAtomic.swift.o \
+.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTestMain.swift.o \
 -target \
 aarch64-unknown-linux-gnu \
 -v \
@@ -249,12 +250,12 @@ aarch64-unknown-linux-gnu \
 /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/lib
 
 /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/bin/swift-autolink-extract \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicBool.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicValue.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/ManagedAtomic.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTestMain.swift.o \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicBool.swift.o \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicValue.swift.o \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/ManagedAtomic.swift.o \
+.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTestMain.swift.o \
 -o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/SwiftAtomicsTest.autolink
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/SwiftAtomicsTest.autolink
 
 # Linking Sources/SwiftAtomicsTest/SwiftAtomicsTest
 
@@ -268,18 +269,18 @@ echo "Linking Sources/SwiftAtomicsTest/SwiftAtomicsTest"
 -Xlinker \
 /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/lib/swift/linux \
 /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/lib/swift/linux/aarch64/swiftrt.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicBool.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicValue.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/ManagedAtomic.swift.o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTestMain.swift.o \
-@/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/Atomics.build/SwiftAtomicsTest.autolink \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicBool.swift.o \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/AtomicValue.swift.o \
+.build/aarch64-unknown-linux-gnu/release/Atomics.build/ManagedAtomic.swift.o \
+.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest.build/SwiftAtomicsTestMain.swift.o \
+@.build/aarch64-unknown-linux-gnu/release/Atomics.build/SwiftAtomicsTest.autolink \
 -L \
 /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/lib/swift/linux \
 -lswiftCore \
 --target=aarch64-unknown-linux-gnu \
 -v \
 -L \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release \
+.build/aarch64-unknown-linux-gnu/release \
 -L \
 /home/ubuntu/swift-5.8-RELEASE-ubuntu20.04-aarch64/usr/lib \
 -Xlinker \
@@ -291,4 +292,4 @@ main=SwiftAtomicsTest_main \
 -Xlinker \
 '-rpath=$ORIGIN' \
 -o \
-/home/ubuntu/swift_atomics_test/.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest
+.build/aarch64-unknown-linux-gnu/release/SwiftAtomicsTest
